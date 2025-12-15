3a98e74b4d489b02bf451d27549e5c556e17a387
Fagogenis99 <sdi2100199@di.uoa.gr>
2025-12-15 20:37:09 +0200

    searching for existing dialogs or creating a new one

--- message.c
+++ message.c
@@ -45,7 +45,7 @@ int main(int argc, char *argv[]){
         // clean everything
         shm->latest_message_id = 0;
         shm->total_users = 0; 
-        for(int i=0;i<MAX_DIALOGS;i++){
+        for(int i=0; i<MAX_DIALOGS; i++){
             shm->dialogs[i].id = 0;   // mark all dialog slots as free
             shm->dialogs[i].user_count = 0;
         }
@@ -54,9 +54,55 @@ int main(int argc, char *argv[]){
             shm->msgs[i].readers_left = 0;
         }
     }else{
+        struct sembuf lock = {0, -1, 0};   // Wait/Lock (-1)
+        struct sembuf unlock = {0, 1, 0};  // Signal/Unlock (+1)
+
+        // lock semaphore before accessing shared memory
+        if(semop(semid, &lock, 1)==-1){
+            perror("semop lock failed");
+            exit(1);
+        }
+
+        // check if dialog exists
+        int dialog_index = -1;
+        for(int i=0; i<MAX_DIALOGS; i++){
+            if(shm->dialogs[i].id == dialog_id){ // check if dialog exists
+                dialog_index = i;
+                break;
+            }
+        }
+
+        if (dialog_index==-1){ // if dialog does not exist, create it
+            for (int i=0; i<MAX_DIALOGS; i++){
+                if (shm->dialogs[i].id==0){ // find free slot
+                    shm->dialogs[i].id = dialog_id;
+                    // shm->dialogs[i].user_count = 1;
+                    dialog_index = i;
+                    printf("Created new dialog with ID %d\n", dialog_id);
+                    break;
+                }
+            }
+        }
+        if (dialog_index==-1){
+            printf("No free dialog slots available\n");
+            if(semop(semid, &unlock, 1)==-1){
+                perror("semop unlock failed");
+                exit(1);
+            }
+            exit(1);
+        }
+        shm->dialogs[dialog_index].user_count++; //update user counts
+        shm->total_users++;
+        printf("Joined dialog %d. Users in dialog: %d. Total users: %d\n", dialog_id, shm->dialogs[dialog_index].user_count, shm->total_users);
         
+        int next_expected_id = shm->latest_message_id + 1; // next message id to read
+        // unlock semaphore
+        if(semop(semid, &unlock, 1)==-1){
+            perror("semop unlock failed");
+            exit(1);
+        }
+
+
     }
-
-
     return 0;
 }
